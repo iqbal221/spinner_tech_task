3d5ddf2f25c5cbd10cd76afc2b4c8a2d5c476ed8
zahed <zahediiuc221@gmail.com>
2023-02-14 20:58:06 +0600

    few correction

--- src/pages/Cart/OrderCard.jsx
+++ src/pages/Cart/OrderCard.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 
 const OrderCard = ({ product }) => {
-  const saveData = JSON.parse(localStorage.getItem("cart"));
-  const handleRemove = (id) => {
-    const RemainProduct = saveData.filter((pdt) => pdt.id !== id);
-
-    localStorage.setItem("cart", JSON.stringify(RemainProduct));
-    // console.log(saveDataString);
-    // localStorage.removeItem(saveDataString);
-  };
   return (
     <div className="card w-[600px] card-side bg-base-100 hover:shadow-violet-500 shadow-md cursor-pointer p-3">
       <figure>
@@ -19,12 +11,7 @@ const OrderCard = ({ product }) => {
         <p className="font-bold text-xl ">Stock: {product?.stock}</p>
         <p className="text-xl font-semibold">Price: Tk.{product?.price}</p>
         <div className="card-actions justify-end">
-          <button
-            onClick={() => handleRemove(product.id)}
-            className="btn btn-error"
-          >
-            Delete
-          </button>
+          <button className="btn btn-error">Delete</button>
         </div>
       </div>
     </div>
